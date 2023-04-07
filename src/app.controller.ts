098cc0751f8a6eb4ab00604d3d6d16e9ccd626ff
alex-fudy <aleksei.vasilev@fudy.eu>
2023-04-07 12:21:21 +0300

    init commit

---
+++ src/app.controller.ts
@@ -0,0 +1,30 @@
+import { Controller, Get, Post } from '@nestjs/common';
+import { AppService } from './app.service';
+
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const Inspector = require('inspector-api');
+
+@Controller()
+export class AppController {
+  private readonly inspector = new Inspector();
+
+  constructor(private readonly appService: AppService) {}
+
+  @Get()
+  getHello(): string {
+    return this.appService.getHello();
+  }
+
+  @Post('/profile/cpu')
+  async makeCPUProfile() {
+    await this.inspector.profiler.enable();
+    await this.inspector.profiler.start();
+
+    await new Promise((resolve) => setTimeout(resolve, 5000));
+
+    const profileBuffer = await this.inspector.profiler.stop();
+    await this.inspector.profiler.disable();
+
+    return profileBuffer;
+  }
+}
